33371e4120ee2e3168d6dbc75abf0fc7ac883677
Govardhan Chaudhari <govardhanchaudhari@gmail.com>
2020-05-10 17:53:13 +0530

    Update RemoveFuncParam_IMP.js

--- RemoveFuncParam_IMP.js
+++ RemoveFuncParam_IMP.js
@@ -1,4 +1,4 @@
-const f = (n,...a)=>curryN(a.length,(...b)=>remove(0,n,b));
-const parametersToRemove=2;
+const f = (n)=>curry((...b)=>remove(0,n,b));
+const parametersToRemove=3;
 const removeTwoParameters = f(parametersToRemove);
 removeTwoParameters(1,2,3,4) // [3,4]
